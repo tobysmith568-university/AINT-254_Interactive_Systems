58b98ef71ed71713313dc68f9f5493d1a11d7621
Toby's Laptop <tobysmith568@hotmail.co.uk>
2017-10-06 18:11:30 +0100

    Extracted week 1

---
+++ Week 1/Assets/Scripts/CharBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ISS
+{
+    public class CharBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_speed = 5.0f;
+
+        [SerializeField]
+        private float m_rotationSpeed = 50.0f;
+
+        Transform m_transform;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_transform = GetComponent<Transform>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float rotY = m_transform.rotation.eulerAngles.y;
+
+            m_transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotY + m_rotationSpeed * 
+                Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f));
+
+            m_transform.position += m_transform.forward * m_speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        }
+    }
+}
---
+++ Week 1/Assets/Scripts/Group.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using Assets.Scripts;
+
+public class Group : MonoBehaviour
+{
+    private CanvasGroup group;
+
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private Slider slider;
+    
+    void Start()
+    {
+        group = GetComponent<CanvasGroup>();
+
+        group.alpha = 0;
+
+        SetAmbientvolume(80f);
+        slider.value = 80f;
+
+        SoundManager.SetManagers(audioMixer);
+    }
+
+    public void SetAmbientvolume(float volume)
+    {
+        audioMixer.SetFloat("AmbientVolume", volume - 80);
+        text.text = volume + "%";
+    }
+
+	void Update()
+    {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(group.alpha == 0)
+            {
+                group.alpha = 1;
+                SoundManager.MenuMode();
+            }
+            else
+            {
+                group.alpha = 0;
+                SoundManager.GameMode();
+            }
+        }
+	}
+}
