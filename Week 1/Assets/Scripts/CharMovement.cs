69e60b7a5bc8b906adf9bc598c313735354576ac
Toby Smith <tobysmith568@hotmail.co.uk>
2017-10-10 15:01:49 +0100

    Worked on swimming

---
+++ Week 1/Assets/Scripts/CharMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour {
+
+    Transform trans;
+    Rigidbody body;
+
+    [SerializeField]
+    int vertRotationSpeed;
+    [SerializeField]
+    int horaRotationSpeed;
+
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        body = GetComponent<Rigidbody>();
+        //body.velocity = new Vector3(0, 0, 5);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            trans.Rotate(Vector3.right * vertRotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            trans.Rotate(-Vector3.right * vertRotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            trans.Rotate(Vector3.forward * horaRotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            trans.Rotate(-Vector3.forward * horaRotationSpeed * Time.deltaTime);
+    }
+}
