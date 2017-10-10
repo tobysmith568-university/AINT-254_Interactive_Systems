b0922040ca26937e24ae74390988f23705a0c94d
Toby Smith <tobysmith568@hotmail.co.uk>
2017-10-10 13:47:47 +0100

    Week 3 learning

---
+++ Week 1/Assets/Scripts/ColourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColourScript : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 customPosition;
+
+    void Start()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Color.red;
+    }
+
+    void Update()
+    {
+        transform.position = customPosition;
+    }
+}
